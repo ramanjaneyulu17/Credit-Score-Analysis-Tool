537da35c8521ca1322cf9cb5d39542c43d1f961a
ramanjaneyulu17 <ramanjaneyulu17.m@gmail.com>
2025-04-17 20:04:46 +0530

    set up done

---
+++ data-collection-service/src/main/java/com/developer/data_collection_service/DataCollectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.developer.data_collection_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCollectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCollectionServiceApplication.class, args);
+	}
+
+}
---
+++ data-collection-service/src/test/java/com/developer/data_collection_service/DataCollectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.data_collection_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
